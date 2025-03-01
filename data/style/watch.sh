682975de09bb0aed6b540d74a75a2eda8cf13ccf
JumpyVi <jumpy634@gmail.com>
2025-03-01 20:11:39 +0000

    refactor: :wastebasket: Remove depreacated styles

--- data/style/watch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-sass \
-    ./variants/elementary-dark.scss:./dist/elementary-dark.css \
-    ./variants/elementary-light.scss:./dist/elementary-light.css \
-    --no-source-map \
