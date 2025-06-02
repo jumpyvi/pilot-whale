18b260dbe34e755221f7b541f3c37f51d14e0fc2
Dralexgon <99817451+Dralexgon@users.noreply.github.com>
2025-06-02 16:17:33 +0200

    package build

--- flake.nix
+++ flake.nix
@@ -31,11 +31,17 @@
       stdenv.mkDerivation {
         pname = "pilot-whale";
         version = "4.2.3";
+
         src = fetchFromGitHub {
           owner = "Dralexgon";
           repo = "pilot-whale";
           rev = "master";
-          sha256 = "sha256-B5JDHSgvoA+YAov/GOJkNXMiex0ZY9HVJOgfyUlpZwY=";
+          sha256 = "sha256-citc8pHMkeUMKDj/WSeZVEMU+nWuELjnPMNCiNwD9XI=";
+        };
+
+        meta = with lib; {
+          description = "Whaler is a tool to manage your Docker containers and images with a graphical interface.";
+          platforms = platforms.linux;
         };
 
         nativeBuildInputs = [
@@ -44,28 +50,20 @@
           pkg-config
           ninja
           python3
-          makeWrapper
+          glib
+          desktop-file-utils
+          wrapGAppsHook4 # Fix glib schemas
         ];
+
         buildInputs = [
           gtk4
           libadwaita
           json-glib
           libgee
-          desktop-file-utils
           curl.dev
         ];
 
-        meta = with lib; {
-          description = "Whaler is a tool to manage your Docker containers and images with a graphical interface.";
-          platforms = platforms.linux;
-        };
-
-        postInstall = ''
-          echo "Recompiling GSettings schemas for runtime use..."
-          glib-compile-schemas $out/share/glib-2.0/schemas
-          echo "Path to GSettings schemas: $out/share/gsettings-schemas/$name"
-          wrapProgram "$out/bin/com.github.sdv43.whaler" --prefix XDG_DATA_DIRS : $out/share
-        '';
+        # Build the application with meson by default
       }
     ;
   };
